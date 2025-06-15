c8e23cdb9c985b2794758b5f8c6c7199b1bda28e
sambhavgsharma <sambhavgsharma70@gmail.com>
2025-06-16 03:51:40 +0530

    Debug 6

--- src/main/java/com/organizeu/organizeu/OrganizeuApplication.java
+++ src/main/java/com/organizeu/organizeu/OrganizeuApplication.java
@@ -2,12 +2,19 @@ package com.organizeu.organizeu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class OrganizeuApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrganizeuApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(OrganizeuApplication.class, args);
+		Environment env = context.getEnvironment();
+
+		System.out.println("🔍 DB_URL = " + env.getProperty("DB_URL"));
+		System.out.println("🔍 DB_USERNAME = " + env.getProperty("DB_USERNAME"));
+		System.out.println("🔍 DB_PASSWORD = " + env.getProperty("DB_PASSWORD"));
 	}
 
 }
